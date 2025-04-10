789eeaf4aeec0e025a9734320c2ee39f29dd58c6
mirzohidd <mirzohidmurodov77@gmail.com>
2025-04-10 21:23:39 +1000

    feat:meta tags integration on document private file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,23 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <meta charSet="UTF-8" />
+      <meta name="title" content="Nestar demo" />
+      <meta name="robots" content="index,follow" />
+      <meta
+        name="keyboard"
+        content={"nestar,nestar.uz,deve,mern,mern nestjs fullstack"}
+      />
+     
+      <link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
+      {/* SEO */}
+      <meta
+        name="description"
+        content={
+          "Buy and sell properties anywhere anytime in Australia. | " +
+          "Покупайте и продавайте недвижимость в любочй точке и в любое время Австралий"
+        }
+      />
       <body>
         <Main />
         <NextScript />
--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -1,4 +1,6 @@
 const PropertyList = () => {
+  console.log("PROPERTY Component -- PAGER ROUTER");
+
   return <div>Property List</div>;
 };
 export default PropertyList;
